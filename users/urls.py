e44e7f546ed1a699587f89dc84c6868f761ff38c
Arya Banaeizadeh <arya11.b76@gmail.com>
2019-01-16 18:00:11 +0330

    + user auth

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,12 @@
 from rest_framework.urlpatterns import format_suffix_patterns
 
 from . import views
-from django.urls import path
+from django.urls import path, re_path, include
+from rest_framework.routers import DefaultRouter
+router = DefaultRouter()
+router.register("", views.UserViewSet)
 urlpatterns = [
-    path('', views.GetUser.as_view()),
+    # path('', views.GetUser.as_view()),
+    path('', include(router.urls)),
 ]
-urlpatterns = format_suffix_patterns(urlpatterns)
+# urlpatterns = format_suffix_patterns(urlpatterns)
--- users/views.py
+++ users/views.py
@@ -1,19 +1,17 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from rest_framework import generics, viewsets, permissions, filters
-from django.contrib.auth.models import User as UserDetail
+from rest_framework import generics, viewsets, filters
 from rest_framework.authentication import TokenAuthentication
-from rest_framework.decorators import api_view
-from rest_framework.views import APIView
-
+from users import permissions
 from users.serializer import UserSerializer
 from .models import User
+from rest_framework import viewsets, filters, status
 
 class GetUser(generics.ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-class UserProfileViewSet(viewsets.ModelViewSet):
+class UserViewSet(viewsets.ModelViewSet):
     """Handles creating and updating profiles"""
 
     serializer_class = UserSerializer
     queryset = User.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
